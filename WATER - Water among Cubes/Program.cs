696b2877a253b013af35c23e92d0058f4d7ca9f2
eziseagal <dominikkubacka@gmail.com>
2024-05-08 00:49:23 +0200

    watercubes

--- WATER - Water among Cubes/Program.cs
+++ WATER - Water among Cubes/Program.cs
@@ -1,89 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Program
+class Program
 {
-    public static void Main()
+    static void Main()
     {
-        try
+        int t = int.Parse(Console.ReadLine());
+        for (int _ = 0; _ < t; _++)
         {
-            int t = int.Parse(Console.ReadLine());
-            while (t-- > 0)
+            if (_ > 0)
+                Console.ReadLine();
+
+            string[] dimensions = Console.ReadLine().Split();
+            int n = int.Parse(dimensions[0]);
+            int m = int.Parse(dimensions[1]);
+
+            int[,] heights = new int[n, m];
+            for (int i = 0; i < n; i++)
             {
-                string[] dimensions = Console.ReadLine().Split();
-                int n = int.Parse(dimensions[0]);
-                int m = int.Parse(dimensions[1]);
-
-                int[,] grid = new int[n, m];
-                int[,] maxLeft = new int[n, m];
-                int[,] maxRight = new int[n, m];
-                int[,] maxUp = new int[n, m];
-                int[,] maxDown = new int[n, m];
-
-                for (int i = 0; i < n; ++i)
+                string[] row = Console.ReadLine().Split();
+                for (int j = 0; j < m; j++)
                 {
-                    string[] heights = Console.ReadLine().Split();
-                    for (int j = 0; j < m; ++j)
-                    {
-                        grid[i, j] = int.Parse(heights[j]);
-                    }
+                    heights[i, j] = int.Parse(row[j]);
                 }
+            }
 
-                for (int i = 0; i < n; ++i)
+            Console.WriteLine(CalculateWaterVolume(heights, n, m));
+        }
+    }
+
+    static int CalculateWaterVolume(int[,] heights, int n, int m)
+    {
+        var visited = new bool[n, m];
+        var pq = new PriorityQueue<(int, int, int), int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            pq.Enqueue((i, 0, heights[i, 0]), heights[i, 0]);
+            pq.Enqueue((i, m - 1, heights[i, m - 1]), heights[i, m - 1]);
+            visited[i, 0] = visited[i, m - 1] = true;
+        }
+        for (int j = 1; j < m - 1; j++)
+        {
+            pq.Enqueue((0, j, heights[0, j]), heights[0, j]);
+            pq.Enqueue((n - 1, j, heights[n - 1, j]), heights[n - 1, j]);
+            visited[0, j] = visited[n - 1, j] = true;
+        }
+
+        int[] dx = { 0, 1, 0, -1 };
+        int[] dy = { 1, 0, -1, 0 };
+        int totalWater = 0;
+
+        while (pq.Count > 0)
+        {
+            var (x, y, h) = pq.Dequeue();
+
+            for (int d = 0; d < 4; d++)
+            {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx, ny])
                 {
-                    int maxHeight = 0;
-                    for (int j = 0; j < m; ++j)
-                    {
-                        maxHeight = Math.Max(maxHeight, grid[i, j]);
-                        maxLeft[i, j] = maxHeight;
-                    }
+                    visited[nx, ny] = true;
+                    int waterHeight = Math.Max(h, heights[nx, ny]);
+                    totalWater += Math.Max(0, h - heights[nx, ny]);
+                    pq.Enqueue((nx, ny, waterHeight), waterHeight);
                 }
-
-                for (int i = 0; i < n; ++i)
-                {
-                    int maxHeight = 0;
-                    for (int j = m - 1; j >= 0; --j)
-                    {
-                        maxHeight = Math.Max(maxHeight, grid[i, j]);
-                        maxRight[i, j] = maxHeight;
-                    }
-                }
-
-                for (int j = 0; j < m; ++j)
-                {
-                    int maxHeight = 0;
-                    for (int i = 0; i < n; ++i)
-                    {
-                        maxHeight = Math.Max(maxHeight, grid[i, j]);
-                        maxUp[i, j] = maxHeight;
-                    }
-                }
-
-                for (int j = 0; j < m; ++j)
-                {
-                    int maxHeight = 0;
-                    for (int i = n - 1; i >= 0; --i)
-                    {
-                        maxHeight = Math.Max(maxHeight, grid[i, j]);
-                        maxDown[i, j] = maxHeight;
-                    }
-                }
-
-                int totalWater = 0;
-                for (int i = 0; i < n; ++i)
-                {
-                    for (int j = 0; j < m; ++j)
-                    {
-                        int minHeight = Math.Min(maxLeft[i, j], Math.Min(maxRight[i, j], Math.Min(maxUp[i, j], maxDown[i, j])));
-                        totalWater += minHeight - grid[i, j];
-                    }
-                }
-
-                Console.WriteLine(totalWater);
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
+
+        return totalWater;
     }
 }
